47c5c1729ca48aafca0177040b54db1f175213df
AndyRuz <157067687+AndyRuz@users.noreply.github.com>
2025-11-18 21:57:05 -0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-P2P Video Streaming Application - Main Entry Point
-Kivy-based user interface with Python networking backend
-"""
-
-from KivyUI import P2PVideoStreamApp
-
-if __name__ == '__main__':
-    P2PVideoStreamApp().run()
